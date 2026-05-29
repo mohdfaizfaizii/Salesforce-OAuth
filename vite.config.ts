06e04e6faf307d9cea7c9ad99f8bd94a201f3ca2
Mohd Faiz <mohdfaiz1472002@gmail.com>
2026-05-29 19:37:03 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
     tanstackStart(),
     viteReact(),
     tsconfigPaths(),
+    cloudflare(),
   ],
   server: {
     port: 5000,
